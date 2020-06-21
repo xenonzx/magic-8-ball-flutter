fc4d2f3659cae900715e58c9c874de8d8b634eb2
Ahmed EL-Dakhakhny <>
2020-06-21 09:49:10 +0200

    displaying random message

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,53 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text("Ask Me Anything"),
       ),
+      body: Ball(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+            child: FlatButton(
+              child: Image.asset('images/ball$ballNumber.png'),
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(4) + 1;
+                });
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
